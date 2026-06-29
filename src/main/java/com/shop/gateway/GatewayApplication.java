e60413fa6b98878bf79417a0d4b85162a16ddd0d
Mandervain <kmnski.l@gmail.com>
2026-06-29 17:50:34 +0200

    feat: add startup logging in main application classes

--- src/main/java/com/shop/gateway/GatewayApplication.java
+++ src/main/java/com/shop/gateway/GatewayApplication.java
@@ -1,12 +1,17 @@
 package com.shop.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(GatewayApplication.class);
+
     public static void main(String[] args) {
+        log.info("Starting Shop Gateway application");
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
